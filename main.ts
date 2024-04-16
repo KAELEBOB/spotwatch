35c1439d9c6944d343af994029a561be1ee801c8
KAELEBOB <167229246+KAELEBOB@users.noreply.github.com>
2024-04-16 18:04:31 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,9 @@
+let teller = 0
+basic.showString("" + (teller))
+for (let index = 0; index < 60; index++) {
+    teller += 1
+    basic.showString("" + (teller))
+}
+basic.forever(function () {
+	
+})
